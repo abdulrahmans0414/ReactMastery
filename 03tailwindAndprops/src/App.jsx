638cdb4a700432d270cbf387b6af9ed70846c434
Abdul Rahman <abdulrahmans0414@gmail.com>
2024-07-04 15:10:43 +0530

    add setup of tailwindcss

---
+++ 03tailwindAndprops/src/App.jsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+<h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+<div className="relative h-[400px] w-[300px] rounded-md ">
+      <img
+        src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+        alt="AirMax Pro"
+        className="z-0 h-full w-full rounded-md object-cover"
+      />
+      <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+      <div className="absolute bottom-4 left-4 text-left">
+        <h1 className="text-lg font-semibold text-white">Delba</h1>
+        <p className="mt-2 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?
+        </p>
+        <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+          View Profile &rarr;
+        </button>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
